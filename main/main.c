bba051099fc49dc346946e5b27808c16b1d652d7
Matt <74658447+iammattderbyshire@users.noreply.github.com>
2025-08-27 22:57:38 +0100

    flashing light works

--- main/main.c
+++ main/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
+#define BLINK_GPIO 22 // Lyrat Mini 1.1 onboard LED
 
 void app_main(void)
 {
+    // gpio_pad_select_gpio(BLINK_GPIO); // Not needed in ESP-IDF v4.x+
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
     int i = 0;
     while (1) {
-        printf("[%d] Hello world!\n", i);
+        // Turn LED on
+        gpio_set_level(BLINK_GPIO, 1);
+        printf("[%d] LED ON - Hello world!\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        // Turn LED off
+        gpio_set_level(BLINK_GPIO, 0);
+        printf("[%d] LED OFF\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
         i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
